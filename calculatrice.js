eae0a75740ee5c4fc9f4c16727946a4277f7e4ba
takxz <corentin.leys@gmail.com>
2025-11-05 15:27:55 +0100

    afficher nombre et clear

--- calculatrice.js
+++ calculatrice.js
@@ -2,8 +2,33 @@ const buttonClear = document.querySelector('.clear');
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('.equals');
+const display = document.getElementById('display');
 
 let number1 = "";
 let number2 = "";
 let operator = "";
 let result = "";
+
+function updateDisplay(){
+    
+}
+
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const displayValue = btn.dataset.number;
+        if (display.textContent === '0') {
+            display.textContent = displayValue;
+        } else {
+            display.textContent += displayValue;
+        }
+    });
+});
+
+buttonClear.addEventListener('click', () => {
+    display.textContent = '0';
+    number1 = "";
+    number2 = "";
+    operator = "";
+    result = "";
+});
+
