8e724f78c1f06c48f666bab471eb92129b9686e1
takxz <corentin.leys@gmail.com>
2025-11-05 15:02:02 +0100

    def des boutton operand

---
+++ calculatrice.js
@@ -0,0 +1,9 @@
+const buttonClear = document.querySelector('.clear');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('.equals');
+
+let number1 = "";
+let number2 = "";
+let operator = "";
+let result = "";
